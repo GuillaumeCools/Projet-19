6280994959aa9172a94bd97af66d5e5a202b2dfe
Guillaume Cools <guillaumecools@Guillaumes-MacBook-Air-2.local>
2023-11-07 10:42:50 +0100

    ajout

--- printf/includes/ft_printf.h
+++ printf/includes/ft_printf.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcools <gcools@student.42.fr>              +#+  +:+       +#+        */
+/*   By: guillaumecools <guillaumecools@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 11:18:42 by gcools            #+#    #+#             */
-/*   Updated: 2023/11/06 15:23:10 by gcools           ###   ########.fr       */
+/*   Updated: 2023/11/07 09:58:05 by guillaumeco      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 # include "libft.h"
 # include <stdlib.h>
 # include <unistd.h>
-# include <limits.h> // a retirer !!!
 
 int		ft_printf(const char *string, ...);
 int		ft_putnbr_pos_return(unsigned int n, int fd);
@@ -26,9 +25,10 @@ int		ft_num_to_hex_up_return(unsigned int n, int fd);
 int		ft_putchar_return(char c, int fd);
 int		ft_putstr_return(char *s, int fd);
 int		ft_putnbr_return(int n, int fd);
+
 size_t	ft_strlen(const char *s);
 int		ft_intlen_original(int n);
-int		ft_intlen_hex(uintptr_t n);
 int		ft_intlen_pos(unsigned int n);
+int		ft_intlen_hex(uintptr_t n);
 
 #endif
